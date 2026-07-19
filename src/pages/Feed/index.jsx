e486338519026f94023b40585170d11a1a63498c
iRafah <irafah.alves@gmail.com>
2026-07-18 23:07:35 -0300

    Updading index

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,9 +1,17 @@
 import { AppLayout } from "../../layouts/App"
 import { CardPost } from "../../components/CardPost"
-import { posts } from "./data"
 import styles from './feed.module.css'
+import { useEffect, useState } from "react"
 
 export const Feed = () => {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:/blog-posts')
+            .then(response => response.json())
+            .then(data => setPosts(data))
+    }, [])
     return (
         <AppLayout>
             <main className={styles.grid}>
